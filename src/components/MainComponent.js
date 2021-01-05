b3d3f487dddf3f265b81777fcd81ad2a392158c3
VagDimitrop <57943789+VagDimitrop@users.noreply.github.com>
2021-01-05 19:52:05 +0200

    Added route to about us page view

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import {DISHES} from '../shared/dishes';
 import {LEADERS} from '../shared/leaders';
@@ -40,7 +41,6 @@ class Main extends Component {
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
           comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
         />
-
       );
     }
 
@@ -52,6 +52,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
