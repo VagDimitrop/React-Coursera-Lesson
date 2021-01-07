aef0a2dccda98db95cd2aa24eb37a803afd8c1bd
VagDimitrop <57943789+VagDimitrop@users.noreply.github.com>
2021-01-07 13:28:44 +0200

    Implemented AboutComponent.js | Assignment 2

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -4,9 +4,24 @@ import { Link } from 'react-router-dom';
 
 function About(props) {
 
+  function RenderLeader({leader}) {
+    return(
+      <div key={leader.id} className="col-12 mt-5">
+        <Media tag="li">
+          <Media object src={leader.image} alt={leader.name}/>
+          <Media body className="ml-5">
+            <Media heading>{leader.name}</Media>
+              <p>{leader.designation}</p>
+              <p>{leader.description}</p>
+          </Media>
+        </Media>
+      </div>
+    );
+  }
+
     const leaders = props.leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
+          <RenderLeader leader={leader} />
         );
     });
 
