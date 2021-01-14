a19a186439bdb51622af304cff75c28db2e5392f
VagDimitrop <57943789+VagDimitrop@users.noreply.github.com>
2021-01-14 19:55:49 +0200

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button,
-  Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input, Col, Row} from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button,
+  Modal, ModalHeader, ModalBody, Label, Col, Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent'
 
   function RenderDish({dish}) {
 
@@ -57,8 +58,25 @@ import { Control, LocalForm, Errors} from 'react-redux-form';
     }
 
     const DishDetail = (props) => {
-
-      if (props.dish != null) {
+      if (props.isLoading) {
+          return(
+            <div className="container">
+              <div className="row">
+                <Loading />
+              </div>
+            </div>
+          );
+      }
+      else if (props.errMess) {
+        return(
+          <div className="container">
+            <div className="row">
+              <h4> {props.errMess}</h4>
+            </div>
+          </div>
+        );
+      }
+      else if (props.dish != null) {
         return(
           <div className="container">
           <div className="row">
@@ -79,7 +97,8 @@ import { Control, LocalForm, Errors} from 'react-redux-form';
             </div>
           </div>
         );
-      } else {
+      }
+      else {
         return(
           <div></div>
         );
